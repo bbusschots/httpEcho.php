e1f7c92ca0e4fcd4b355cac9a220d94b8b92e034
Bart Busschots <opensource@bartificer.net>
2019-04-04 00:51:55 +0100

    First fully functional version

--- httpEcho.php
+++ httpEcho.php
@@ -1,7 +1,7 @@
 <?php
 
-// TEMP WHILE DEBUGING - REMOVE LATER: enable error display
-ini_set('display_errors', 1);
+// DEBUGGING ONLY: enable error display
+// ini_set('display_errors', 1);
 
 // workaround for the fact the NGINX + FPM does not provider a getallheaders() function
 // Credit: https://www.popmartian.com/tipsntricks/2015/07/14/howto-use-php-getallheaders-under-fastcgi-php-fpm-nginx-etc/
@@ -33,10 +33,10 @@ if (!function_exists('getallheaders')) {
             <h1>Client</h1>
             
             <dl>
-                <dt>IP</dt>
-                <dd><code><?php echo($_SERVER['REMOTE_ADDR']); ?></code></dd>
-                <dt>Browser (User Agent String)</dt>
-                <dd><code><?php echo($_SERVER['HTTP_USER_AGENT']); ?></code></dd>
+              <dt>IP</dt>
+              <dd><code><?php echo($_SERVER['REMOTE_ADDR']); ?></code></dd>
+              <dt>Browser (User Agent String)</dt>
+              <dd><code><?php echo($_SERVER['HTTP_USER_AGENT']); ?></code></dd>
             </dl>
         </section>
     </div>
@@ -58,15 +58,72 @@ if (!function_exists('getallheaders')) {
                 <dt>Headers <span class="badge badge-pill badge-primary"><?php echo(sizeof($requestHeaderNames)); ?></span></dt>
                 <dd>
                     <?php if(sizeof($requestHeaderNames) < 1){ ?>
-                        No headers were passed.
+                        <p class="text-muted">The request contained no headers.</p>
                     <?php }else{ ?>
-                        <ul>
-                            <?php foreach($requestHeaderNames as $headerName){ ?>
-                                <li><code><?php echo($headerName); ?>: <?php echo($requestHeaders[$headerName]); ?></code></li>
-                            <?php } ?>
+                        <ul class="list-unstyled">
+                          <?php foreach($requestHeaderNames as $headerName){ ?>
+                              <li><code><?php echo($headerName); ?>: <?php echo($requestHeaders[$headerName]); ?></code></li>
+                          <?php } ?>
                         </ul>
                     <?php } ?>
                 </dd>
+                <dt>Query String</dt>
+                <dd>
+                    <?php if($_SERVER['QUERY_STRING']){ ?>
+                        <code><?php echo($_SERVER['QUERY_STRING']); ?></code>
+                    <?php }else{ ?>
+                        <p class="text-muted">The request did not contain a query string.</p>
+                    <?php } ?>
+                </dd>
+                <?php
+                $formData = $_GET + $_POST;
+                $formDataNames =  array_keys($formData);
+                ?>
+                <dt>Form Data <span class="badge badge-pill badge-primary"><?php echo(sizeof($formDataNames)); ?></span></dt>
+                <dd>
+                    <?php if(sizeof($formDataNames) < 1){ ?>
+                        <p class="text-muted">The request contained no form data.</p>
+                    <?php }else{ ?>
+                        <dl>
+                          <?php foreach($formDataNames as $dataName){ ?>
+                              <dt><code><?php echo($dataName); ?></code></dt>
+                              <dd><code><?php echo($formData[$dataName]); ?></code></dd>
+                          <?php } ?>
+                        </dl>
+                    <?php } ?>
+                </dd>
+                <?php
+                $cookieNames =  array_keys($_COOKIE);
+                ?>
+                <dt>Cookies <span class="badge badge-pill badge-primary"><?php echo(sizeof($_COOKIE)); ?></span></dt>
+                <dd>
+                    <?php if(sizeof($cookieNames) < 1){ ?>
+                        <p class="text-muted">The request contained no cookies.</p>
+                    <?php }else{ ?>
+                        <dl>
+                          <?php foreach($cookieNames as $cookieName){ ?>
+                              <dt><code><?php echo($cookieName); ?></code></dt>
+                              <dd><code><?php echo($_COOKIE[$cookieName]); ?></code></dd>
+                          <?php } ?>
+                        </dl>
+                    <?php } ?>
+                </dd>
+            </dl>
+        </section>
+    </div>
+    <div class="row">
+        <section class="col">
+            <h1>Server</h1>
+            
+            <dl>
+              <dt>IP:Port</dt>
+              <dd><code><?php echo($_SERVER['SERVER_ADDR'].':'.$_SERVER['SERVER_PORT']); ?></code></dd>
+              <dt>Name</dt>
+              <dd><code><?php echo($_SERVER['SERVER_NAME']); ?></code></dd>
+              <dt>Software</dt>
+              <dd><code><?php echo($_SERVER['SERVER_SOFTWARE']); ?></code></dd>
+              <dt>CGI Revision</dt>
+              <dd><code><?php echo($_SERVER['GATEWAY_INTERFACE']); ?></code></dd>
             </dl>
         </section>
     </div>
